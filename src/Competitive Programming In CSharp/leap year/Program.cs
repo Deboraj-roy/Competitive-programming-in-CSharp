a32d07f3352db622f5a7c8e3dfc8e6cb3e06b159
deboraj-roy <deborajroy123@gmail.com>
2024-03-07 13:09:06 +0600

    checking leap year

---
+++ src/Competitive Programming In CSharp/leap year/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Certainly! Below is a simple C# program that checks if a given year is a leap year. A year is considered a leap year if it is divisible by 4 but not by 100, unless it is also divisible by 400. This means that years like 2000 and 2020 are leap years, but 1900 and 2100 are not.
+*/
+
+Console.Write("Enter a year to check if it's a leap year or not: ");
+int year = int.Parse(Console.ReadLine());
+
+if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+{
+    Console.WriteLine($"{year} is a leap year.");
+}
+else
+{
+    Console.WriteLine($"{year} is not a leap year.");
+}
