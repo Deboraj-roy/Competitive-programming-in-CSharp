b7184cfe17d1de2f607d25ba33b827d6fcdfad46
“DEBORAJ <deborajroy123@gmail.com>
2024-07-21 00:55:19 +0600

    Task 4 Done

---
+++ src/Competitive Programming In CSharp/Task 4/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_4;
+/*
+Task 4
+Write a class called Box which contains three member variables; length, width and height. This class also contains
+two constructors, one empty and another one is with parameter. You also have to write a function named
+getArea() that calculates and returns the area of a box object. Write necessary code inside the main function to
+test the class by creating an object.*/
+namespace Task_4
+{
+    public class Box
+    {
+        double length, width, height;
+        public Box()
+        {
+        }
+        public Box(double a, double b, double c)
+        {
+            this.length = a;
+            this.width = b;
+            this.height = c;
+        }
+        public double getArea()
+        {
+            return 2*((length * width) + (width * height) + (height * length));
+        }
+    }
+}
---
+++ src/Competitive Programming In CSharp/Task 4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*using System.ComponentModel;
+
+Task 4
+Write a class called Box which contains three member variables; length, width and height. This class also contains
+two constructors, one empty and another one is with parameter. You also have to write a function named
+getArea() that calculates and returns the area of a box object. Write necessary code inside the main function to
+test the class by creating an object.*/
+
+using Task_4;
+
+Box box = new Box();
+Console.WriteLine("Area of Box: " + box.getArea());
+Console.WriteLine("*************************************************");
+Console.Write("Length:");
+var length = double.Parse(Console.ReadLine());
+Console.Write("Width:");
+var width = double.Parse(Console.ReadLine());
+Console.Write("Height:");
+var height = double.Parse(Console.ReadLine());
+Box box2 = new Box(length, width, height);
+Console.WriteLine("Area of Box: " + box2.getArea());
