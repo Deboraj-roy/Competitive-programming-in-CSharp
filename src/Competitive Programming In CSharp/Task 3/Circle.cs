f84f6a191e6dfc964e5cf7a0b89ca14051e80cc4
“DEBORAJ <deborajroy123@gmail.com>
2024-07-21 00:35:48 +0600

    Task 3 Done

---
+++ src/Competitive Programming In CSharp/Task 3/Circle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class Circle
+    {
+        double radius;
+        bool set = false;
+        public void setRadius(double r)
+        { 
+            radius = r; 
+            set = true;
+        }
+
+        public void getArea()
+        {
+            if (set)
+            {
+                double Area = Math.PI * radius * radius;
+                Console.WriteLine("Area: " + Area);
+            }
+            else
+            {
+                setRadius();
+                getArea();
+            }
+        }
+        public void getCircumference()
+        {
+            if (set)
+            {
+                double Circumference = 2 * Math.PI * radius;
+                Console.WriteLine("Circumference: " +  Circumference);
+            }
+            else
+            {
+                setRadius();
+                getCircumference();
+            }
+        } 
+
+        void setRadius()
+        {
+            Console.Write("Enter Radius: ");
+            var r = double.Parse(Console.ReadLine());
+            //var e = double.Pi;
+            setRadius(r);
+        }
+
+    }
+}
---
+++ src/Competitive Programming In CSharp/Task 3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Task 3
+Write a class called Circle, that will have only one variable named radius. A function named setRadius will set the
+radius of the circle. The class should have two other functions, one should output the circumference and the another
+should output the area of that circle.
+Hints :
+Area = PI * r2
+Circumference = 2 * PI * r
+*/
+
+using Task_3;
+
+Circle circle = new Circle(); 
+circle.getArea();
+circle.getCircumference();
+
+Console.WriteLine("********************************************************************");
+Circle circle2 = new Circle();
+Console.Write("Enter Radius: ");
+var r = double.Parse(Console.ReadLine());
+circle2.setRadius(r);
+circle2.getArea();
+circle2.getCircumference();
+
