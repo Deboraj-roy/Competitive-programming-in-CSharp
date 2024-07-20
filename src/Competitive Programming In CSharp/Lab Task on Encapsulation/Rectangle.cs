b916341fbcd4cbc7075e697047f3864e25237960
“DEBORAJ <deborajroy123@gmail.com>
2024-07-20 18:30:55 +0600

    Rectangle class of Lab Task on Encapsulation

---
+++ src/Competitive Programming In CSharp/Lab Task on Encapsulation/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_on_Encapsulation
+{
+    public class Rectangle
+    {
+        int height, width;
+        public setDimension(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        void showArea()
+        {
+            Console.WriteLine($"Area: {height * width}");
+        }
+        int getArea() { return height * width; }
+    }
+}
